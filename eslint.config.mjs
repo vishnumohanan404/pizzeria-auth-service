e34274f0f80d5ec2bd4d3bca30568689e948bbf7
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-01-07 22:22:17 +0530

    update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,13 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    ignores: ["dist", "node_modules", "jest.config.js", "**/*.mjs", "test"],
-  },
+  ...tseslint.configs.recommendedTypeChecked,
+  { ignores: ["dist", "node_modules", "eslint.config.mjs"] },
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
-        allowDefaultProject: true,
       },
     },
     rules: {
