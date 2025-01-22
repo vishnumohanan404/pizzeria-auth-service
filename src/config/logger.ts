20085ed7c9a3b21b92a223ceb9e32efedbaf6896
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-01-22 22:40:35 +0530

    fix logger timestamp

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -6,33 +6,25 @@ const logger = winston.createLogger({
   defaultMeta: {
     serviceName: "auth-service",
   },
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
   transports: [
     new winston.transports.File({
       level: "info",
       dirname: "logs",
       filename: "combined.log",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.File({
       level: "error",
       dirname: "logs",
       filename: "error.log",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.Console({
       level: "info",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
       silent: Config.NODE_ENV === "test",
     }),
   ],
