50c2af9c9781a15812da1a1a280b6dcf13798218
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-02-06 15:12:10 +0530

    add auth middleware on getTenantById

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -34,7 +34,9 @@ router.patch(
     tenantController.update(req, res, next),
 );
 router.get("/", (req, res, next) => tenantController.getAll(req, res, next));
-router.get("/:id", (req, res, next) => tenantController.getOne(req, res, next));
+router.get("/:id", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+  tenantController.getOne(req, res, next),
+);
 router.delete(
   "/:id",
   authenticate,
